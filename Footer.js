94cad501164455667055ce11026e7373afd4f023
Conbrown100 <54599157+Conbrown100@users.noreply.github.com>
2022-03-29 10:42:04 -0500

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Footer.css';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+      
+      <section class='social-media'>
+        <div class='social-media-wrap'>
+          <div class='footer-logo'>
+            <Link to='/' className='social-logo'>
+              StockHelper
+              <i class='fab fa-typo3' />
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../App.css';
+import Cards from '../Cards';
+import HeroSection from '../HeroSection';
+import Footer from '../Footer';
+import { ChartComponent } from '../Graph.js';
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
